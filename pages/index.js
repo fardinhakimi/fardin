e9ad12bf3720078cf016123d21902873fb5c96e4
fardinhakimi <fardinhakimi@gmail.com>
2021-10-15 22:39:37 +0200

    update website

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,6 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1><a href="https://www.linkedin.com/in/fardin-hakimi-79742b84/" target="blank"> Fardin Hakimi</a></h1>
-      <h3> Powered By:</h3>
-      <ul>
-        <li>Nextjs</li>
-        <li>Github</li>
-        <li>Github Actions</li>
-        <li>Docker</li>
-        <li>DockerHub</li>
-        <li>Nginx</li>
-        <li>DigitalOcean</li>
-      </ul>
     </div>
   )
 }
