625d45f92aee6ff2c2c7ba5155c0879bf145aeea
fardinhakimi <fardinhakimi@gmail.com>
2021-10-16 20:03:56 +0200

    do you work?

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1><a href="https://www.linkedin.com/in/fardin-hakimi-79742b84/" target="blank"> Fardin Hakimi</a></h1>
+      <h1><a href="https://www.linkedin.com/in/fardin-hakimi-79742b84/" target="blank">Hakimi</a></h1>
     </div>
   )
 }
